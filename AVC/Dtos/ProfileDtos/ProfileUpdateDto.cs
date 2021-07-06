7be93685e2136a02d199782fe7b0357d04b3c8d0
Triệu Minh Huy (K13) <huytmse130336@fpt.edu.vn>
2021-07-06 14:25:14 +0700

    add image field in car list

--- AVC/Dtos/ProfileDtos/ProfileUpdateDto.cs
+++ AVC/Dtos/ProfileDtos/ProfileUpdateDto.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -8,6 +9,8 @@ namespace AVC.Dtos.ProfileDtos
 {
     public class ProfileUpdateDto
     {
+        [MaxLength(11)]
+        [Phone]
         public string Phone { get; set; }
         public IFormFile AvatarImage { get; set; }
     }
