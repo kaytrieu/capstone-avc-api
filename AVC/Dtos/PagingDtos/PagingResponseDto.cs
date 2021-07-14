f9f5da1c2bd41a8200a00cbadbe93b2f137dfd85
Triệu Minh Huy (K13) <huytmse130336@fpt.edu.vn>
2021-07-14 13:20:05 +0700

    Update paging dto have next / previous page number and total page of limit

--- AVC/Dtos/PagingDtos/PagingResponseDto.cs
+++ AVC/Dtos/PagingDtos/PagingResponseDto.cs
@@ -12,6 +12,15 @@ namespace AVC.Dtos.PagingDtos
         {
             Result = dto.AsQueryable().Paging(page, limit).AsEnumerable();
             Count = dto.Count();
+            if ((double)dto.Count() / limit > page)
+            {
+                NextPageNumber = page + 1;
+            }
+
+            if (page > 1)
+                PreviousPageNumber = page - 1;
+
+            TotalPage = (int)Math.Ceiling((double)dto.Count() / limit);
         }
 
         public PagingResponseDto()
@@ -22,5 +31,8 @@ namespace AVC.Dtos.PagingDtos
         public int Count { get; set; }
         public string NextPage { get; set; }
         public string PreviousPage { get; set; }
+        public int? NextPageNumber { get; set; }
+        public int? PreviousPageNumber { get; set; }
+        public int? TotalPage { get; set; }
     }
 }
